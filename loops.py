e612f4da5a0ff755db827317f1dca71ac1a3b843
RakeshBhugra-Vurbalize <rakesh@vurbalize.com>
2025-08-02 11:57:51 +0530

    checkpoint

---
+++ loops.py
@@ -0,0 +1,18 @@
+# print numbers from 1 to 10
+
+# print('1,2,3,4,5,6,7,8,9,10')
+
+# print numbers from 1 to 100
+# for i in range(1, 101):
+#     print(i, end=', ')
+
+
+# print numbers from 1 to 10 using a loop
+
+# 1 <= number < 11
+
+for i in range(1, 11):
+    print(i)
+
+for i in range(1, 14):
+    print(i)
