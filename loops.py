ad5b04b9b096288c6047ac2c67f34d2e1522ee61
RakeshBhugra-Vurbalize <rakesh@vurbalize.com>
2025-08-02 12:08:59 +0530

    checkpoint

--- loops.py
+++ loops.py
@@ -1,18 +1,32 @@
-# print numbers from 1 to 10
+# # print numbers from 1 to 10
 
-# print('1,2,3,4,5,6,7,8,9,10')
+# # print('1,2,3,4,5,6,7,8,9,10')
 
-# print numbers from 1 to 100
-# for i in range(1, 101):
-#     print(i, end=', ')
+# # print numbers from 1 to 100
+# # for i in range(1, 101):
+# #     print(i, end=', ')
 
 
-# print numbers from 1 to 10 using a loop
+# # print numbers from 1 to 10 using a loop
 
-# 1 <= number < 11
+# # 1 <= number < 11
 
-for i in range(1, 11):
-    print(i)
+# # for i in range(1, 11):
+# #     print(i)
 
-for i in range(1, 14):
-    print(i)
+
+items = ["1", True, 3.13]
+
+# items.append("new item")
+
+# for item in items:
+#     print(item)
+
+# # enumerate
+# for idx, item in enumerate(items):
+#     print(f"{idx}: {item}")
+
+for i in range(0, 3):
+    print(f"{i}: {items[i]}")
+
+print(len(items))
