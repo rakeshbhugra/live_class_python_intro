1b7293217745a5245a3641624cf16dea08638040
RakeshBhugra-Vurbalize <rakesh@vurbalize.com>
2025-08-02 11:15:40 +0530

    checkpoint

--- conditional_statements.py
+++ conditional_statements.py
@@ -1,9 +1,9 @@
-# user_message: str = "What is the capital of France?"
+user_message: str = "What is the capital of France?"
 
-# if user_message != "":
-#     print("user is allowed to send this message")
-# else:
-#     print("user is not allowed to send this message")
+if user_message != "":
+    print("user is allowed to send this message")
+else:
+    print("user is not allowed to send this message")
 
     
 if "johnn" == "john":
@@ -12,3 +12,23 @@ elif "john" == "john":
     print("second condition is true")
 else:
     print("both conditions are false")
+
+
+name1 = "John"
+name2 = "hanibal"
+
+if name1 == name2:
+    print("First condition is true")
+elif name1.lower() == name2.lower():
+    print("Second condition is true")
+elif name1.upper() == name2.upper():
+    print("Third condition is true")
+elif name1.title() == name2.title():
+    print("Fourth condition is true")
+else:
+    print("Both conditions are false")
+# The above code compares two names with case sensitivity and insensitivity.
+
+
+text_var = "123a"
+print(text_var.isnumeric())
